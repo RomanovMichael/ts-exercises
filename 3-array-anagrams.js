ef3edeb5c9cbcbc1be9d1ed2aa1e2b5101e41f31
Romanov Michael <romanov.players@gmail.com>
2025-03-19 16:34:12 +0300

    feat: add 4-object-types

--- 3-array-anagrams.js
+++
@@ -1,9 +0,0 @@
-// Реализуйте функцию filterAnagrams(), которая находит все анаграммы слова.
-// Функция принимает исходное слово и список для проверки — массив.
-// А возвращает функция массив всех анаграмм.
-// Если в списке нет анаграммы, то возвращается пустой массив:
-var filterAnagrams = function (word, array) {
-    var sortedWord = function (word) { return word.split('').sort().join(''); };
-    return array.filter(function (el) { return sortedWord(el) === sortedWord(word); });
-};
-console.log(filterAnagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']));
